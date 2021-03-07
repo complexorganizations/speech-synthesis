1913125306f29d91846a57571d007b1fb5e83a2c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-07 07:18:16 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,49 +1,58 @@
 package main
 
 import (
-        "context"
-        "fmt"
-        "io/ioutil"
-        "log"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
 
-        texttospeech "cloud.google.com/go/texttospeech/apiv1"
-        texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+)
+
+var (
+	textFilePath = os.Args[1]
 )
 
 func main() {
-        // Instantiates a client.
-        ctx := context.Background()
-        client, err := texttospeech.NewClient(ctx)
-        if err != nil {
-                log.Fatal(err)
-        }
-        // Perform the text-to-speech request on the text input with the selected
-        // voice parameters and audio file type.
-        req := texttospeechpb.SynthesizeSpeechRequest{
-                // Set the text input to be synthesized.
-                Input: &texttospeechpb.SynthesisInput{
-                        InputSource: &texttospeechpb.SynthesisInput_Text{Text: "Hello, World!"},
-                },
-                // Build the voice request, select the language code ("en-US") and the SSML
-                // voice gender ("neutral").
-                Voice: &texttospeechpb.VoiceSelectionParams{
-                        LanguageCode: "en-US",
-                        SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
-                },
-                // Select the type of audio file you want returned.
-                AudioConfig: &texttospeechpb.AudioConfig{
-                        AudioEncoding: texttospeechpb.AudioEncoding_MP3,
-                },
-        }
-        resp, err := client.SynthesizeSpeech(ctx, &req)
-        if err != nil {
-                log.Fatal(err)
-        }
-        // The resp's AudioContent is binary.
-        filename := "output.mp3"
-        err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
-        if err != nil {
-                log.Fatal(err)
-        }
-        fmt.Printf("Audio content written to file: %v\n", filename)
+	// Read the file
+	content, err := ioutil.ReadFile(textFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Instantiates a client.
+	ctx := context.Background()
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: string(content)},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "en-US",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// The resp's AudioContent is binary.
+	filename := "output.mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
 }
